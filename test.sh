0e5498b1969677bf8062d15063b00110ef071330
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2023-06-02 14:15:50 +0530

    Creating a react app

---
+++ test.sh
@@ -0,0 +1,9 @@
+read -p "Enter the name of your React app: " app_name
+
+
+npx create-react-app $app_name
+
+# Change to the app directory
+cd $app_name
+
+echo "React app '$app_name' created successfully!"
